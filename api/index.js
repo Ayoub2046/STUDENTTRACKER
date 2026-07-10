327ffeaab1c97d4e6ba6cd6d1fc6b9713e6ff7f9
Ayoub2046 <engayoub2@gmail.com>
2026-07-10 18:56:56 +0300

    Add all server deps to root package.json for Vercel resolution

--- api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/api/health', (req, res) => {
-  res.json({ status: 'ok', message: 'Hello from AYUB OS' })
-})
-
-module.exports = app
